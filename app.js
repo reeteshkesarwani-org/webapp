886b9386af3c97b9b55b2dcd54676590bb4569e2
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-03-01 23:41:56 -0500

    Corrected config

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var mysql = require('mysql');
 var express=require('express');
 const routes = require('./routes/routes');
 var app1=express()
-app1.use(express.json())
-const config=require('./config');
+app1.use(express.json());
 require('dotenv').config();
 var db=require('./configuration/sequelize')
 const product=require('./models/user')
