4f7132cdf5cbfd050a1d6bbafa0efbafc3abb61f
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-03-01 23:41:56 -0500

    Corrected config

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -10,10 +10,6 @@ const { S3Client,DeleteObjectCommand, PutObjectCommand } = require("@aws-sdk/cli
 
 const s3 = new S3Client({
   region: process.env.BUCKET_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
 
 const randomImageName = (filename) => {
