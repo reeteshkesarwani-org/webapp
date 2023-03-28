a7810295caba3227f8fc500993a9146d38553d78
reeteshkesarwani <114504936+reeteshkesarwani@users.noreply.github.com>
2023-03-28 17:55:06 -0400

    A7 1 (#33)
    * ami correct (#2)
    
    * adding configuration for clodwatch and metrices and adding logger in API's

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -5,6 +5,12 @@ const Crypto = require("crypto");
 const User = db.user;
 const bcrypt=require('bcrypt');
 var mysql = require("mysql");
+const logger = require("../logger");
+const SDC = require('statsd-client');
+const sdc = new SDC({
+    host: "localhost",
+    port: 8125
+});
 
 const { S3Client,DeleteObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
 
@@ -43,17 +49,20 @@ const authUser = async (req, productId) => {
 
 
 const addImage = async (req, res, next) => {
+   sdc.increment('add/image');
     const auth = req.headers.authorization;
     if (!auth || auth.indexOf('Basic ') === -1) return res.status(403).json("Forbidden Request")
     const base64Credentials = auth.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
+      logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } })
     if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
+      logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
     }
     else {
         let passcheck = false;
@@ -63,6 +72,7 @@ const addImage = async (req, res, next) => {
             passcheck = true;
         }
         if (passcheck == false) {
+          logger.error("Invalid Credentials Details");
             return res.status(401).json('Invalid Credentials');
         }
 
@@ -71,7 +81,8 @@ const addImage = async (req, res, next) => {
   
       const productIdCheck = parseInt(productid);
       if (productIdCheck != productid) {
-        return res.status(403).send({
+        logger.error("Bad Request: Invalid Product Id");
+        return res.status(400).send({
           error: "Bad Request: Invalid Product Id",
         });
       }
@@ -103,29 +114,34 @@ const addImage = async (req, res, next) => {
           date_created: image.dataValues.date_created,
         });
       } else {
+        logger.error("You are not authorized to add images to this product.");
         return res.status(403).json({
           message: "You are not authorized to add images to this product.",
         });
       }
     } catch (error) {
       console.log(error);
+      logger.error("Failed to add product image");
       res.status(400).json({ message: "Failed to add product image" });
     }
   }}
   ;
 
   const getImage = async (req, res, next) => {
+    sdc.increment('get/image');
     const auth = req.headers.authorization;
     if (!auth || auth.indexOf('Basic ') === -1) return res.status(403).json("Forbidden Request")
     const base64Credentials = auth.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
+      logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } })
     if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
+      logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
     }
     else {
         let passcheck = false;
@@ -135,6 +151,7 @@ const addImage = async (req, res, next) => {
             passcheck = true;
         }
         if (passcheck == false) {
+          logger.error("Invalid Credentials");
             return res.status(401).json('Invalid Credentials');
         }
 
@@ -143,6 +160,7 @@ const addImage = async (req, res, next) => {
       
           const productIdCheck = parseInt(productid);
           if (productIdCheck != productid) {
+            logger.error("Bad Request: Invalid Product Id");
             return res.status(400).send({
               error: "Bad Request: Invalid product Id",
             });
@@ -154,6 +172,7 @@ const addImage = async (req, res, next) => {
           console.log(imgIdCheck);
           
           if (imgIdCheck != id) {
+            logger.error("Bad Request: Invalid Image Id");
             return res.status(400).send({
               error: "Bad Request: Invalid Image Id",
             });
@@ -163,17 +182,21 @@ const addImage = async (req, res, next) => {
               where: { product_id: productid, image_id: imageid },
             });
             if (image) {
+              logger.info(image);
               res.json(image);
             } else {
+              logger.error("Image not found");
               res.status(404).json({ message: "Image not found" });
             }
           } else {
+            logger.error("You are not authorized to view images of this product.");
             return res.status(403).json({
               message: "You are not authorized to view images of this product.",
             });
           }
         } catch (error) {
           console.log(error);
+          logger.error("Failed to get product image");
           res.status(400).json({ message: "Failed to get product image" });
         }
       }
@@ -181,69 +204,19 @@ const addImage = async (req, res, next) => {
     ;
 
   const getAllImages = async (req, res, next) => {
+    sdc.increment('getall/image');
     const auth = req.headers.authorization;
     if (!auth || auth.indexOf('Basic ') === -1) return res.status(403).json("Forbidden Request")
     const base64Credentials = auth.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
-    }
-    const usr = await db.user.findOne({ where: { username: usernamegetting } })
-    if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
-    }
-    else {
-        let passcheck = false;
-        const passwordstoreduser = usr.password;
-        // var hashedPassword = await bcrypt.hash(passwordstoreduser, 10);
-        if (await bcrypt.compare(passwordgetting, passwordstoreduser)) {
-            passcheck = true;
-        }
-        if (passcheck == false) {
-            return res.status(401).json('Invalid Credentials');
-        }
-
-        try {
-          const { productid } = req.params;
-      
-          const productIdCheck = parseInt(productid);
-          if (productIdCheck != productid) {
-            return res.status(400).send({
-              error: "Bad Request: Invalid Product Id",
-            });
-          }
-      
-          if (await authUser(req, productid)) {
-            const images = await Images.findAll({
-              where: { product_id: productid },
-            });
-            if (images) {
-              res.json(images);
-            } else {
-              res.status(404).json({ message: "Images not found" });
-            }
-          } else {
-            return res.status(403).json({
-              message: "You are not authorized to view images of this product.",
-            });
-          }
-        } catch (error) {
-          res.status(400).json({ message: "Failed to get product images" });
-        }
-      }};
-  
-  const deleteImage = async (req, res, next) => {
-    const auth = req.headers.authorization;
-    if (!auth || auth.indexOf('Basic ') === -1) return res.status(403).json("Forbidden Request")
-    const base64Credentials = auth.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const [usernamegetting, passwordgetting] = credentials.split(':');
-    if (!usernamegetting || !passwordgetting) {
+      logger.error("Invalid Authentication Details");
         return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } })
     if (usr === null) {
+      logger.error("Invalid Authentication Details");
         return res.status(400).json('Invalid Authentication Details');
     }
     else {
@@ -254,6 +227,73 @@ const addImage = async (req, res, next) => {
             passcheck = true;
         }
         if (passcheck == false) {
+            logger.error("Invalid Credentials");
+            return res.status(401).json('Invalid Credentials');
+        }
+
+        try {
+          const { productid } = req.params;
+      
+          const productIdCheck = parseInt(productid);
+          if (productIdCheck != productid) {
+            logger.error("Bad Request: Invalid Product Id");
+            return res.status(400).send({
+              error: "Bad Request: Invalid Product Id",
+            });
+          }
+      
+          if (await authUser(req, productid)) {
+            const images = await Images.findAll({
+              where: { product_id: productid },
+            });
+            if (images) {
+              logger.info(images);
+              res.json(images);
+            } else {
+              logger.error("Images not found");
+              res.status(404).json({ message: "Images not found" });
+            }
+          } else {
+            logger.error("You are not authorized to view images of this product.");
+            return res.status(403).json({
+              message: "You are not authorized to view images of this product.",
+            });
+          }
+        } catch (error) {
+          logger.error("Failed to get product images");
+          res.status(400).json({ message: "Failed to get product images" });
+        }
+      }};
+  
+  const deleteImage = async (req, res, next) => {
+    sdc.increment('delete/image');
+    const auth = req.headers.authorization;
+    if (!auth || auth.indexOf('Basic ') === -1)
+    {
+      logger.error("Forbidden Request");
+      return res.status(403).json("Forbidden Request")
+    }
+    const base64Credentials = auth.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [usernamegetting, passwordgetting] = credentials.split(':');
+    if (!usernamegetting || !passwordgetting) {
+        logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
+    }
+    const usr = await db.user.findOne({ where: { username: usernamegetting } })
+    if (usr === null) {
+       logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
+    }
+    else {
+        let passcheck = false;
+        const passwordstoreduser = usr.password;
+        // var hashedPassword = await bcrypt.hash(passwordstoreduser, 10);
+        if (await bcrypt.compare(passwordgetting, passwordstoreduser)) {
+            passcheck = true;
+        }
+        if (passcheck == false) {
+           logger.error("Invalid Credentials");
             return res.status(401).json('Invalid Credentials');
         }
 
@@ -262,6 +302,7 @@ const addImage = async (req, res, next) => {
       
           const productIdCheck = parseInt(productid);
           if (productIdCheck != productid) {
+            logger.error("Bad Request: Invalid Product Id");
             return res.status(400).send({
               error: "Bad Request: Invalid Product Id",
             });
@@ -270,6 +311,7 @@ const addImage = async (req, res, next) => {
           const id = req.params.imageid;
           const imgIdCheck = parseInt(id);
           if (imgIdCheck != id) {
+            logger.error("Bad Request: Invalid Image Id");
             return res.status(400).send({
               error: "Bad Request: Invalid Image Id",
             });
@@ -282,6 +324,7 @@ const addImage = async (req, res, next) => {
             });
            
             if (!image) {
+              logger.error("Image not found");
               res.status(404).json({ message: "Image not found" });
             } else {
               var url = image.s3_bucket_path;
@@ -295,14 +338,17 @@ const addImage = async (req, res, next) => {
                 new DeleteObjectCommand(params)
               );
               await image.destroy();
+              logger.info("Image deleted successfully");
               res.status(204).json({ message: "Image deleted successfully" });
             }
           } else {
+            logger.error("You are not authorized to delete images of this product.");
             return res.status(403).json({
               message: "You are not authorized to delete images of this product.",
             });
           }
         } catch (error) {
+          logger.error("Failed to delete image");
           res.status(400).json({ message: "Failed to delete image" });
         }
       }};
--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -5,6 +5,12 @@ var db = require("../configuration/sequelize");
 const { response } = require("express");
 const user = require("../models/user");
 const { NUMBER } = require("sequelize");
+const logger = require("../logger");
+const SDC = require('statsd-client');
+const sdc = new SDC({
+    host: "localhost" ,
+    port: 8125
+});
 
 
 
@@ -38,11 +44,13 @@ exports.createProduct = async function (req, res) {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
+        logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } })
     if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
+        logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
     }
     else {
         let passcheck = false;
@@ -52,6 +60,7 @@ exports.createProduct = async function (req, res) {
             passcheck = true;
         }
         if (passcheck == false) {
+            logger.error("Invalid Credentials Details");
             return res.status(401).json('Invalid Credentials');
         }
 
@@ -65,15 +74,19 @@ exports.createProduct = async function (req, res) {
             var quantity1 = req.body.quantity;
 
             if (!req.body.name || !req.body.description || !req.body.manufacturer || !req.body.quantity || !req.body.sku) {
+                logger.error("data is incomplete please provide value for name,manufacturer, description ,sku and quantity");
                 return res.status(206).send({ message: "data is incomplete please provide value for name,manufacturer, description ,sku and quantity" })
             }
             if (Object.keys(req.body).length > 5) {
+                logger.error("Extra field value provided ,please provide value for first_name,last_name,password and username");
                 return res.status(403).json("Extra field value provided ,please provide value for first_name,last_name,password and username");
             }
             if ((typeof req.body.quantity) != "number") {
+                logger.error("Product Quantity must be number");
                 return res.status(400).json('Product Quantity must be number');
             }
             if (quantity1 > 100 || quantity1 < 1) {
+                logger.error("Product Quantity is greater than 100 or less than 1 please keep between 0-100 or equal");
                 return res.status(400).json('Product Quantity is greater than 100 or less than 1 please keep between 0-100 or equal');
             }
             var owner_user_id1 = usr.id;
@@ -89,6 +102,7 @@ exports.createProduct = async function (req, res) {
 
             );
             await product1.save();
+            logger.info(product1);
             return res.status(201).json(product1);
         }
         else {
@@ -106,25 +120,30 @@ exports.updateProduct = async function (req, res) {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
+        logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } });
     const product1= await db.product.findOne({where:{id:req.params.productid}});
     if(product1===null)
     {
+        logger.error("Product Not Found");
         return res.status(404).json("Product Not Found");
     }
     const productownerid=product1.owner_user_id;
     
     if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
+        logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
     }
     if(productownerid!=usr.id)
     {
+        logger.error("Forbidden request");
         return res.status(403).json('Forbidden request');
     }
     else {
         if (!req.body.name || !req.body.description || !req.body.manufacturer || !req.body.quantity || !req.body.sku) {
+            logger.error("data is incomplete please provide value for name,manufacturer, description, sku and quantity");
             return res.status(206).send({ message: "data is incomplete please provide value for name,manufacturer, description, sku and quantity" })
         }
         let passcheck = false;
@@ -134,30 +153,36 @@ exports.updateProduct = async function (req, res) {
             passcheck = true;
         }
         if (passcheck == false) {
+            logger.error("Invalid Credentials");
             return res.status(401).json('Invalid Credentials');
         }
 
         const p1 = await db.product.findOne({ where: { sku: req.body.sku } });
         const p2 = await db.product.findOne({ where: { id: req.params.productid } });
         if (p2 === null) {
+            logger.error("Product not Found Unable to Update");
             return res.status(404).json('Product not Found Unable to Update');
         }
         if (p1 != null && p1.sku!= p2.sku) {
+            logger.error("Product cant be updated because of same sku value already present in database");
             return res.status(404).json('Product cant be updated because of same sku value already present in database');
         }
         else {
 
             
             if (Object.keys(req.body).length > 5) {
+                logger.error("Extra field value provided ,please provide value for first_name,last_name,password ,sku and username");
                 return res.status(403).json("Extra field value provided ,please provide value for first_name,last_name,password ,sku and username");
             }
 
             // if(req.body.quantity)
             // console.log(typeof (req.body.quantity));
             if ((typeof req.body.quantity) != "number") {
+                logger.error("Product Quantity must be number");
                 return res.status(400).json('Product Quantity must be number');
             }
             if (req.body.quantity > 100 || req.body.quantity < 0) {
+                logger.error("Product Quantity is greater than 100 or less than 0 please keep between 0-100 or equal");
                 return res.status(400).json('Product Quantity is greater than 100 or less than 0 please keep between 0-100 or equal');
             }
             await p2.update({
@@ -168,7 +193,7 @@ exports.updateProduct = async function (req, res) {
                 sku: req.body.sku,
                 quantity: req.body.quantity
             })
-
+            logger.info("product got updated successfully");
             return res.status(201).json('product got updated successfully');
         }
 
@@ -183,21 +208,25 @@ exports.patchProduct = async function (req, res) {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
-        return res.status(401).json("Invalid Authentication Details");
+        logger.error("Invalid Authentication Details");
+        return res.status(403).json("Invalid Authentication Details");
     }
     const usr = await db.user.findOne({ where: { username: usernamegetting } });
     const product1= await db.product.findOne({where:{id:req.params.productid}});
     if(product1===null)
     {
+        logger.error("Product Not Found");
         return res.status(404).json("Product Not Found");
     }
     const productownerid=product1.owner_user_id;
     
     if (usr === null) {
-        return res.status(401).json('Invalid Authentication Details');
+        logger.error("Invalid Authentication Details");
+        return res.status(400).json('Invalid Authentication Details');
     }
     if(productownerid!=usr.id)
     {
+        logger.error("Forbidden request");
         return res.status(403).json('Forbidden request');
     }
     else {
@@ -208,6 +237,7 @@ exports.patchProduct = async function (req, res) {
             passcheck = true;
         }
         if (passcheck == false) {
+            logger.error("Invalid Credentials");
             return res.status(401).json('Invalid Credentials');
         }
         var p1 = null;
@@ -216,9 +246,11 @@ exports.patchProduct = async function (req, res) {
         }
         const p2 = await db.product.findOne({ where: { id: req.params.productid } });
         if (p2 === null) {
+            logger.error("Product not Found Unable to Update");
             return res.status(404).json('Product not Found Unable to Update');
         }
         if (p1 != null && p1.sku != p2.sku ) {
+            logger.error("Product cant be updated because of same sku value");
             return res.status(404).json('Product cant be updated because of same sku value');
         }
 
@@ -252,9 +284,11 @@ exports.patchProduct = async function (req, res) {
             }
             if (req.body.quantity) {
                 if ((typeof req.body.quantity) != "number") {
+                    logger.error("Product Quantity must be number");
                     return res.status(400).json('Product Quantity must be number');
                 }
                 if (req.body.quantity > 100 || req.body.quantity < 1) {
+                    logger.error("Product Quantity is greater than 100 or less than 1 please keep between 0-100 or equal");
                     return res.status(400).json('Product Quantity is greater than 100 or less than 1 please keep between 0-100 or equal');
                 }
                 await p2.update({
@@ -271,9 +305,11 @@ exports.patchProduct = async function (req, res) {
             {
                 if(key!=="name" && key!="description" && key!="sku" && key!="manufacturer" && key!="quantity")
                 {
+                    logger.error("bad request: invalid field in the body");
                     return res.status(400).send({message:"bad request: invalid field in the body"})
                 }
             }
+            logger.info("product got updated successfully");
             return res.status(201).json('product got updated successfully');
         }
 
@@ -282,11 +318,14 @@ exports.patchProduct = async function (req, res) {
 
 
 exports.getProduct = async function (req, res) {
+    sdc.increment('getproduct/product');
     const usr = await db.product.findOne({ where: { id: req.params.productid } })
     if (usr === null) {
+        logger.error("Product not Found");
         return res.status(404).json('Product not Found');
     }
     else {
+        logger.info(usr);
             return res.status(200).json(usr);
     }
 };
@@ -298,6 +337,7 @@ exports.deleteProduct = async function (req, res) {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [usernamegetting, passwordgetting] = credentials.split(':');
     if (!usernamegetting || !passwordgetting) {
+        logger.error("Invalid Authentication Details");
         return res.status(403).json("Invalid Authentication Details");
     }
 
@@ -305,6 +345,7 @@ exports.deleteProduct = async function (req, res) {
     const usr = await db.user.findOne({ where: { username: usernamegetting } })
     // console.log(usr);
     if (usr === null) {
+        logger.error("Invalid Authentication Details");
         return res.status(400).json('Invalid Authentication Details');
     }
     else {
@@ -316,9 +357,11 @@ exports.deleteProduct = async function (req, res) {
         }
         const p11 = await db.product.findOne({ where: { id: req.params.productid } })
         if (p11 === null) {
+            logger.error("Product Not Found");
             return res.status(404).json("Product Not Found");
         }
         if (passcheck == false) {
+            logger.error("Invalid Credentials");
             return res.status(401).json('Invalid Credentials');
         }
         if (p11.owner_user_id == usr.id && passcheck == true) {
@@ -327,9 +370,11 @@ exports.deleteProduct = async function (req, res) {
                     id: req.params.productid
                 }
             })
+            logger.info("product got deleted successfully");
             return res.status(200).json("product got deleted successfully");
         }
         else {
+            logger.error("The entered userid and product created do not match please enter the correct value for the user id");
             return res.status(401).json("The entered userid and product created do not match please enter the correct value for the user id")
         }
 
@@ -338,5 +383,6 @@ exports.deleteProduct = async function (req, res) {
 
 
 exports.health = async function (req, res) {
+    logger.info("server is healthy");
     return res.status(200).json("server is healthy ");
 }
