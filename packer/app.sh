43b687723059dfcac3daaded77cc911457075bb5
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-03-29 15:01:34 -0400

    solving amazon cloudwatch installation issue

--- packer/app.sh
+++ packer/app.sh
@@ -16,11 +16,8 @@ mkdir ~/webapp
 unzip webapp.zip -d ~/webapp
 cd ~/webapp && npm i
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file: ..\cloudwatchconfig.json \
-    -s
+sudo wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
 
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
