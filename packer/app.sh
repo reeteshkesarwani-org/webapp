705e8b4193e5bef1a8190fd18d55ab13495ca4c8
reeteshkesarwani <114504936+reeteshkesarwani@users.noreply.github.com>
2023-03-29 15:03:42 -0400

    A7 3 (#35)
    * ami correct (#2)
    
    * solving amazon cloudwatch installation issue

--- packer/app.sh
+++ packer/app.sh
@@ -16,8 +16,9 @@ mkdir ~/webapp
 unzip webapp.zip -d ~/webapp
 cd ~/webapp && npm i
 
+sudo wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
 
-sudo yum install amazon-cloudwatch-agent
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 sudo systemctl enable webapp.service
