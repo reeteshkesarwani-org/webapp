55c4c00a1735e629ffbab18fc5a8dcf2e0e03162
reeteshkesarwani <114504936+reeteshkesarwani@users.noreply.github.com>
2023-04-12 15:46:13 -0400

    A9 3 (#43)
    * ami correct (#2)
    
    * reverting the files to previous stage

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,6 @@ module.exports = (app) => {
     app.post("/v1/user", control.createUser);
      app.get("/v1/user/:userid", control.getUser);
      app.get("/healthz", control.health);
-     app.get("/health123", control.health123);
      app.put("/v1/user/:userid", control.editUser);
      app.post("/v1/product",productcontroller.createProduct);
      app.get("/v1/product/:productid",productcontroller.getProduct);
